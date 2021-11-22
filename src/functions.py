91e0681115a97f91e4be16a864add280fd7a8cfa
Giovanni Forleo <37332690+pegasusgio@users.noreply.github.com>
2021-11-22 19:49:26 +0100

    Implemented StratifiedKFold for the second lab

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,6 @@
-from pandas import read_csv
+from pandas import read_csv, DataFrame
+from sklearn import model_selection
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 import collections
 import matplotlib.pyplot as plt
 
@@ -8,6 +10,7 @@ def loadCsv(path):
 
 
 def preElaborationData(dataframe, columns):
+    statistics = DataFrame()
     for c in columns:
         print('Column name: ', c)
         statistics = (dataframe[c].describe())
@@ -24,17 +27,52 @@ def removeColumns(dataframe, columns):
     dataframe = dataframe.drop(columns=removedColumns)
     print('Removed columns: ', removedColumns)
     print('Dim before the removal:', shape)
-    print('Dim after the removal:', dataframe.shape)
+    print('Dim after the removal:', dataframe.shape, '\n')
     return dataframe, removedColumns
 
 
 def countLabels(dataframe):
-    c = collections.Counter(dataframe['Label'])
-    return c
+    return collections.Counter(dataframe['Label'])
+    # return dataframe['Label'].value_counts()
 
 
-def printHistogram(dataframe, c):
+def printHistogram(dataframe):
+    c = countLabels(dataframe)
     labels = (list(c))
-    plt.hist(dataframe['Label'], bins=len(labels))
+    plt.hist(dataframe['Label'], len(labels))
     plt.show()
     return
+
+
+def stratifiedKFold(X, y, folds, seed):
+    # This cross - validation object is a variation of KFold that return the stratified folds.
+    # The fold are made by preserving the percentage of samples for each class.
+    skf = model_selection.StratifiedKFold(n_splits=folds, random_state=seed, shuffle=True)
+
+    # empty lists declaration
+    xTrainList = []
+    xTestList = []
+    yTrainList = []
+    yTestList = []
+
+    # looping over split
+    for trainIndex, testIndex in skf.split(X, y):
+        print("TRAIN:", trainIndex, "TEST:", testIndex)
+        xTrainList.append(X.iloc[trainIndex])
+        xTestList.append(X.iloc[testIndex])
+        yTrainList.append(y.iloc[trainIndex])
+        yTestList.append(y.iloc[testIndex])
+    return xTrainList, xTestList, yTrainList, yTestList
+
+'''
+def decisionTreeLearner(X, y, c, ccp, seed):
+    tree = DecisionTreeClassifier(criterion=c, random_state=seed, ccp_alpha=ccp)
+    tree.fit(X, y)
+    return tree
+
+
+def showTree(tree):
+    fig = plt.figure(figsize=(40, 30))
+    plot_tree(tree)
+    plt.show(tree)
+'''
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 # file path
-from functions import loadCsv, preElaborationData, removeColumns, countLabels, printHistogram
+import numpy as np
+
+from functions import loadCsv, preElaborationData, removeColumns, printHistogram, stratifiedKFold, decisionTreeLearner, \
+    showTree
 
 # retrieve the csv file
 path = "C:\\Users\\pegasusgio\\Downloads\\trainDdosLabelNumeric.csv"
@@ -7,17 +10,37 @@ dataframe = loadCsv(path)  # return a DataFrame
 shape = dataframe.shape  # return a tuple representing the dimensionality of the DataFrame.
 
 # print dataframe and others data
-print('The training set observed by the csv file is the following:\n', dataframe)
-print('The matrix size is: ', shape)
-print('The first five rows:\n', dataframe.head())
+print('The training set observed by the csv file is the following:\n', dataframe, '\n')
+print('The matrix size is: ', shape, '\n')
+print('The first five rows:\n', dataframe.head(), '\n')
 print('The attributes labels:\n', dataframe.columns, '\n')
 
 # pre-elaboration
 columns = list(dataframe.columns.values)  # list take an Index type and return attributes' labels as array
 statistics = preElaborationData(dataframe, columns)  # loop over each column and compute describe function
 dataframe, removedColumns = removeColumns(dataframe, columns)  # remove the columns that have same value on min-max
-print('The removed Columns are:', removedColumns)
+print('The removed Columns are:', removedColumns, '\n')
 
 # retrieve cardinality of each class and print an histogram
-c = countLabels(dataframe)  # return a Counter
-printHistogram(dataframe, c)
+printHistogram(dataframe)
+
+# stratified K-fold CV
+cols = list(dataframe.columns.values)   # retrieves all the attribute names
+independentList = cols[0:dataframe.shape[1]-1]   # remove from the cols list named 'Label'
+print('Independent List:', independentList, '\n')
+target = 'Label'
+X = dataframe.loc[:, independentList]  # all dataframe values except for 'Label'
+y = dataframe[target]  # array containing the only label values
+folds = 5  # number of folds
+seed = 42  # value to randomize the random split
+np.random.seed(seed)
+xTrainList, xTestList, yTrainList, yTestList = stratifiedKFold(X, y, folds, seed)
+print('\n', 'The first 5 rows')
+print(dataframe.head())
+
+
+# decision Tree
+'''
+t = decisionTreeLearner(X, y, 'entropy', 0.001, seed)
+showTree(t)
+'''
